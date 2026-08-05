e05e74b06e7448ab05aad5279f4ed8d7ccd6a328
B0netti <7686332+B0netti@users.noreply.github.com>
2026-08-05 14:17:12 +1000

    Render Spotlight beam above team pins

--- Core/Core.lua
+++ Core/Core.lua
@@ -2,8 +2,8 @@ local addonName, BattleMaps = ...
 
 _G.BattleMaps = BattleMaps
 BattleMaps.addonName = addonName
-BattleMaps.VERSION = "2.6.47"
-BattleMaps.BUILD = "2.6.47-spotlight-fov"
+BattleMaps.VERSION = "2.6.48"
+BattleMaps.BUILD = "2.6.48-beam-over-team"
 
 BattleMaps.COLORS = {
     red = { 0.77, 0.17, 0.16 },
